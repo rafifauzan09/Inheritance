7b9bba6c0089700cb336c6265620ae23f55f4962
rafifauzan09 <rafif.almahdy@students.amikom.ac.id>
2020-04-11 20:11:19 +0700

    rest

---
+++ program/inheritance.cs
@@ -0,0 +1,68 @@
+using System;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Fauzan", 16);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Budi Agnoerdi", 36, "190345", "Pancasila");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Rafif Fauzan Almahdy", 16, "19.11.2742", "rafif.almahdy@students.amikom.ac.id");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name = "no name", int age = 0)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void GetNameAndAge()
+        {
+            Console.WriteLine("The person has name: {0} and age: {1}", Name, Age);
+        }
+    }
+
+    public class Teacher : Person
+    {
+        
+        public string TeacherId { get; set; }
+        public string Subject { get; set; }
+
+        public Teacher(string name = "no name", int age = 0, string teacherId = "no id", string subject = "no subject")
+        {
+            Name = name;
+            Age = age;
+            TeacherId = teacherId;
+            Subject = subject;
+        }
+    }
+    
+    public class Student : Person
+    {
+        public string StudentId { get; set; }
+        public string Email { get; set; }
+
+        public Student(string name = "no name", int age = 0, string studentId = "no id", string email = "no email")
+        {
+            Name = name;
+            Age = age;
+            StudentId = studentId;
+            Email = email;
+        }
+    }
+
+}
